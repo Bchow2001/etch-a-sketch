98e1bf1f53ce4e116db2315f472e410e15d0ef19
Bryan Chow <bchow.post@gmail.com>
2023-03-20 21:00:48 +0000

    Add hover effect

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const gridContainer = document.querySelector(".grid-container");
 
 // Div creation function
 function createDiv() {
-    const childDiv = document.createElement("div");
-    childDiv.classList.add("grid-square");
-    childDiv.setAttribute("style", "border: 2px solid red; height: 10px; width: 10px;");
-    gridContainer.appendChild(childDiv);
+    const pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    pixel.setAttribute("style", "height: 10px; width: 10px;");
+    pixel.addEventListener('mouseover', changeColor);
+    pixel.addEventListener('mousedown', changeColor);
+    gridContainer.appendChild(pixel);
 }
 
 let gridSize = parseInt(prompt("How big should the grid be?", 16))
@@ -14,19 +16,17 @@ for(let i = 0; i < (gridSize * gridSize); i++){
     createDiv();
 }
 
-const gridSquare = document.querySelector(".grid-square")
+const gridSquare = document.querySelector(".grid-square");
 
-function coloring(e){
-    console.log(e);
-    gridSquare.classList.add("pixel");
+let mouseDown = false;
+
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function changeColor(e) {
+    if (e.type === "mouseover" && !mouseDown) {
+        return;
+    } else {
+        e.target.style.backgroundColor = "#000000";
+    }
 }
-    
-
-// Listens for mouse down then adds mouse over event listener
-gridContainer.addEventListener("mousedown", () => {
-    gridContainer.addEventListener("mouseover", coloring);
-  });
-// On mouseup remove listener for mouse over
-window.addEventListener("mouseup", () => {
-    gridContainer.removeEventListener("mouseover", coloring);
-  });
